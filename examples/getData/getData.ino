4aeefad4faa3799844388b5921d252eb875c4af8
Asuki Kono <asukiaaa@gmail.com>
2017-08-05 21:35:47 +0900

    Support esp32-idf on example

--- examples/getData/getData.ino
+++ examples/getData/getData.ino
@@ -1,12 +1,25 @@
 #include <MPU9250_asukiaaa.h>
 
+#ifdef _ESP32_HAL_I2C_H_
+#define SDA_PIN 26
+#define SCL_PIN 25
+#endif
+
 MPU9250 mySensor;
 
 void setup() {
   while(!Serial);
-  Serial.begin(115200);
 
+  Serial.begin(115200);
+  Serial.println("started");
+
+#ifdef _ESP32_HAL_I2C_H_
+  // for esp32
+  Wire.begin(SDA_PIN, SCL_PIN); //sda, scl
+#else
   Wire.begin();
+#endif
+
   mySensor.setWire(&Wire);
 
   mySensor.beginAccel();
